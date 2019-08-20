08eca9a456bbc879f7bba8d0668baf03cc754bb8
sharpevb <victoriabsharpe@gmail.com>
2019-08-20 17:14:35 -0400

    attempt number 2 at linking htmls

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("./app/public"));
 
 // Routes
 require("./app/routing/apiRoutes.js")(app);
